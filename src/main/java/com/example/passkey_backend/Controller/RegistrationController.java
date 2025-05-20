e9a30733450f24c1bad6cfe0578640c2bd77e24e
Lunis <lunis1@live.nl>
2025-05-20 13:44:27 +0200

    New branch

---
+++ src/main/java/com/example/passkey_backend/Controller/RegistrationController.java
@@ -0,0 +1,4 @@
+package com.example.passkey_backend.Controller;
+
+public class RegistrationController {
+}
--- src/main/java/com/example/passkey_backend/config/SecurityConfig.java
+++ src/main/java/com/example/passkey_backend/config/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.example.passkey_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,11 +16,22 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .formLogin(withDefaults())
-                .webAuthn(webAuthn -> webAuthn
-                        .rpName("My Relying Party")
-                        .rpId("localhost")
-                        .allowedOrigins("https://localhost:8443")
+                .webAuthn((webAuthn) -> webAuthn
+                        .rpName("Spring Security Relying Party")
+                        .rpId("example.com")
+                        .allowedOrigins("https://example.com")
                 );
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
